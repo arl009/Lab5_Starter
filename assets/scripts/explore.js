dadd685d981291bb8bcbb96087cb4fcae7cdee4b
arl009 <97709149+arl009@users.noreply.github.com>
2023-05-08 19:22:19 -0700

    text to speech for explore

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,23 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const synth = window.speechSynthesis;
+	const voiceSelect = document.getElementById("voice-select");
+  let voices = [];
+
+  const expose = document.getElementById("explore");
+	const button = explore.querySelector("button");
+  const image = explore.querySelector("img");
+
+  function populateVoiceList() {
+    voices = synth.getVoices();
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.value = i;
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+      voiceSelect.appendChild(option);
+    }
+  }
+
+  populateVoiceList();
 }
